d7cdfb92cde49c4ed004f951bb45f4b2aa43e0c3
kimgajin <kimgajin1@naver.com>
2025-11-08 23:02:49 +0900

    fix : import

--- src/pages/auth/components/SignupForm.jsx
+++ src/pages/auth/components/SignupForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useAuth } from '../../../../contexts/AuthContext'
+import { useAuth } from '../../../contexts/AuthContext'
 
 const REGIONS = [
   '대전광역시',
